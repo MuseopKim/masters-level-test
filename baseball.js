8dabd1a0aff1546eff9afc7fda9913827d48bd41
museopkim0214@gmail.com <49878687+MuseopKim@users.noreply.github.com>
2019-12-02 16:50:03 +0900

    Add handling exception and annotation
    예외나 에러 상황을 차단하는 코드를 추가 하였고 각 역할을 하는 함수와 메소드에 관한 주석을 첨가 하였습니다.

--- baseball.js
+++ baseball.js
@@ -1,26 +1,24 @@
 const match = {};
 
+// 팀 생성자 함수
 function Team(name, pitcher) {
   this.name = name;
   this.batterPlayers = [];
   this.pitcher = pitcher;
 }
 
+// 팀에 선수를 등록
 Team.prototype.addPlayer = function(name, avrg) {
   const average = Number(avrg).toFixed(3);
 
-  if (average > 0.1 && average < 0.5) {
-    this.batterPlayers.push({
-      num: this.batterPlayers.length + 1,
-      name: name,
-      average: average
-    });
-    // } else {
-    //   alert("0.1부터 0.5 사이의 값을 입력 해주세요.");
-    // }
-  }
+  this.batterPlayers.push({
+    num: this.batterPlayers.length + 1,
+    name: name,
+    average: average
+  });
 };
 
+// 사용자 입력 값을 받아와 적합한지 검사한 뒤 객체에 추가
 Team.prototype.setBatter = function(num) {
   let batterNames;
   let batterAvrgs;
@@ -35,42 +33,73 @@ Team.prototype.setBatter = function(num) {
   for (let i = 0; i < 9; i++) {
     const batterName = batterNames[i].value;
     const batterAvrg = batterAvrgs[i].value;
-    this.addPlayer(batterName, batterAvrg);
+    const average = Number(batterAvrg);
+
+    if (batterName.length === 0) {
+      batterNames[i].focus();
+      alert("이름을 입력 해주세요.");
+      break;
+    }
+
+    if (average > 0.1 && average < 0.5) {
+      this.addPlayer(batterName, average);
+    } else {
+      alert("0.1에서 0.5 사이의 값을 입력 해주세요.");
+      batterAvrgs[i].focus();
+      break;
+    }
   }
 };
 
-// 1. 사용자가 정보를 입력
-// 2. 버튼을 클릭하면 정보를 가져옴 (객체 생성 => 피쳐 입력 => 배터들 입력)
-
+// 사용자가 입력한 팀 이름, 투수 이름을 받아와 팀 1을 생성
 function setTeam1() {
-  const teamName = document.querySelector("#jsTeam1Name").value;
-  const teamPitcher = document.querySelector("#jsTeam1PitcherName").value;
+  const inputTeamName = document.querySelector("#jsTeam1Name");
+  const inputTeamPitcher = document.querySelector("#jsTeam1PitcherName");
+  const teamName = inputTeamName.value;
+  const teamPitcher = inputTeamPitcher.value;
 
-  const team1 = new Team(teamName, teamPitcher);
-
-  team1.setBatter(1);
-
-  return team1;
+  if (!teamName) {
+    alert("팀명을 입력 해주세요.");
+    inputTeamName.focus();
+  } else if (!teamPitcher) {
+    alert("투수명을 입력 해주세요.");
+    teamPitcher.focus();
+  } else {
+    const team1 = new Team(teamName, teamPitcher);
+    team1.setBatter(1);
+    return team1;
+  }
 }
 
+// 사용자가 입력한 팀 이름, 투수 이름을 받아와 팀 2를 생성
 function setTeam2() {
-  const teamName = document.querySelector("#jsTeam2Name").value;
-  const teamPitcher = document.querySelector("#jsTeam2PitcherName").value;
+  const inputTeamName = document.querySelector("#jsTeam2Name");
+  const inputTeamPitcher = document.querySelector("#jsTeam2PitcherName");
+  const teamName = inputTeamName.value;
+  const teamPitcher = inputTeamPitcher.value;
 
-  const team2 = new Team(teamName, teamPitcher);
-  team2.setBatter(2);
-
-  return team2;
+  if (!teamName) {
+    alert("팀명을 입력 해주세요.");
+    inputTeamName.focus();
+  } else if (!teamPitcher) {
+    alert("투수명을 입력 해주세요.");
+    teamPitcher.focus();
+  } else {
+    const team2 = new Team(teamName, teamPitcher);
+    team2.setBatter(2);
+    return team2;
+  }
 }
 
+// 현재 회차의 스탯을 초기화
 match.resetStats = function() {
-  console.log(this);
   this.strike = 0;
   this.ball = 0;
   this.out = 0;
   this.hit = 0;
 };
 
+// 입력된 팀1, 팀2 정보를 받아와 경기를 생성
 match.setMatch = function(team1, team2) {
   this.team1 = team1;
   this.team2 = team2;
@@ -93,6 +122,7 @@ match.setMatch = function(team1, team2) {
     team1.name + " VS " + team2.name + "의 시합을 시작합니다.";
 };
 
+// 클릭시 생성한 팀, 경기를 저장
 function saveData() {
   const team1 = setTeam1();
   console.log(team1);
@@ -104,13 +134,89 @@ function saveData() {
 
   const inputBox = document.querySelector("#jsInputBox");
   const infoBox = document.querySelector("#jsInfoBox");
-
+  const showInputBtn = document.querySelector("#jsShowInputBtn");
   showInfo();
 
+  showInputBtn.innerHTML = "처음으로 돌아가기";
+  showInputBtn.removeEventListener("click", showAndHideBox);
+  showInputBtn.addEventListener("click", function() {
+    location.reload();
+  });
   inputBox.style.display = "none";
   infoBox.style.display = "block";
 }
 
+// 저장된 정보를 바탕으로 팀과 선수 정보를 출력
+function showInfo() {
+  // 팀 1 정보 출력
+  const team1 = match.team1;
+  const team1Name = document.querySelector("#jsInfoTeam1Name");
+  const team1PlayerName = document.querySelectorAll(".jsInfoTeam1PlayerName");
+  const team1PlayerAvrg = document.querySelectorAll(".jsInfoTeam1PlayerAvrg");
+  const team1PitcherName = document.querySelector("#jsInfoTeam1PitcherName");
+
+  team1Name.innerHTML = team1.name;
+
+  for (let i = 0; i < team1PlayerName.length; i++) {
+    const player = team1.batterPlayers[i];
+    team1PlayerName[i].innerHTML = player.name;
+    team1PlayerAvrg[i].innerHTML = player.average;
+  }
+
+  team1PitcherName.innerHTML = team1.pitcher;
+
+  // 팀 2 정보 출력
+  const team2 = match.team2;
+  const team2Name = document.querySelector("#jsInfoTeam2Name");
+  const team2PlayerName = document.querySelectorAll(".jsInfoTeam2PlayerName");
+  const team2PlayerAvrg = document.querySelectorAll(".jsInfoTeam2PlayerAvrg");
+  const team2PitcherName = document.querySelector("#jsInfoTeam2PitcherName");
+
+  team2Name.innerHTML = team2.name;
+
+  for (let i = 0; i < team2PlayerName.length; i++) {
+    const player = team2.batterPlayers[i];
+    team2PlayerName[i].innerHTML = player.name;
+    team2PlayerAvrg[i].innerHTML = player.average;
+  }
+
+  team2PitcherName.innerHTML = team2.pitcher;
+}
+
+// 버튼 클릭에 따라 화면 변화를 통제
+function showAndHideBox(e) {
+  const infoBox = document.querySelector("#jsInfoBox");
+  const playBox = document.querySelector("#jsPlayBox");
+  const inputBox = document.querySelector("#jsInputBox");
+
+  if (e.target.id === "jsShowInputBtn") {
+    infoBox.style.display = "none";
+    playBox.style.display = "none";
+    inputBox.style.display = "block";
+  } else if (e.target.id === "jsShowInfoBtn") {
+    let isFilled = true;
+    for (let i = 0; i < 9; i++) {
+      if (!match.team1.batterPlayers[i] || !match.team2.batterPlayers[i]) {
+        isFilled = false;
+      }
+    }
+
+    if (isFilled) {
+      playBox.style.display = "none";
+      inputBox.style.display = "none";
+      infoBox.style.display = "block";
+      showInfo();
+    } else {
+      alert("팀 정보를 먼저 입력 해주세요.");
+    }
+  } else if (e.target.id === "jsShowPlayBtn") {
+    inputBox.style.display = "none";
+    infoBox.style.display = "none";
+    playBox.style.display = "block";
+  }
+}
+
+// 경기 진행중 타율을 매개변수로 받아 판정을 결정
 function decideJudgement(hitAvrg) {
   const randomNum = Math.random();
   let judgement = "";
@@ -128,6 +234,7 @@ function decideJudgement(hitAvrg) {
   return judgement;
 }
 
+// 회말 시에 공수를 전환
 match.changeOffend = function() {
   if (this.isSecondHalf) {
     this.round++;
@@ -141,19 +248,23 @@ match.changeOffend = function() {
   this.resetStats();
 };
 
+// 경기 종료시 결과화면을 출력
 function setFinish() {
   const playBox = document.querySelector("#jsPlayBox");
   playBox.style.display = "none";
 
   const resultBox = document.querySelector("#jsResultBox");
   resultBox.style.display = "block";
+
+  const menu = document.querySelector("#jsMenuBox");
+  menu.style.display = "none";
 }
 
+// 3 아웃시 경기를 종료 시킬지 여부를 결정
 function isFinish() {
   if (match.round === 5 && match.isSecondHalf) {
     if (match.team1Score < match.team2Score) {
       setFinish();
-      console.log("Game Over");
     } else {
       match.resetStats();
       match.changeOffend();
@@ -166,6 +277,7 @@ function isFinish() {
   }
 }
 
+// 판정을 바탕으로 스탯에 반영
 match.updateStats = function(judgement) {
   const commentBox = document.querySelector("#jsJudgement");
   const offendTeam = this.offendTeam;
@@ -209,15 +321,13 @@ match.updateStats = function(judgement) {
   commentBox.innerHTML = comment;
 };
 
+// 판정을 실행
 function judgeStats(hitAvrg) {
   const judgement = decideJudgement(hitAvrg);
   match.updateStats(judgement);
 }
 
-// 1. 코멘트
-// 2. 현재 선수
-// 3. 판정
-
+// 경기 상황을 실시간으로 노출
 function printStats(num, player) {
   const comment = document.querySelector("#jsComment");
   const playerInfo = document.querySelector("#jsPlayerInfo");
@@ -238,6 +348,7 @@ function printStats(num, player) {
   countOut.innerHTML = match.out;
 }
 
+// 최종 경기 결과 출력
 function showResult() {
   const team1Name = document.querySelector("#jsTableTeam1Name");
   const team2Name = document.querySelector("#jsTableTeam2Name");
@@ -251,6 +362,7 @@ function showResult() {
   team2Score.innerHTML = match.team2.score;
 }
 
+// 버튼을 클릭하면 투구를 실행
 function playRound() {
   const currentTeam = match.offendTeam;
   const currentNum = match.currentNum;
@@ -270,67 +382,6 @@ function playRound() {
   );
   console.log(`Team1 : ${match.team1.score}`, `Team2 : ${match.team2.score}`);
   console.log(match.round);
-}
-
-function showInfo() {
-  // 팀 1 정보 입력
-  const team1 = match.team1;
-  const team1Name = document.querySelector("#jsInfoTeam1Name");
-  const team1PlayerName = document.querySelectorAll(".jsInfoTeam1PlayerName");
-  const team1PlayerAvrg = document.querySelectorAll(".jsInfoTeam1PlayerAvrg");
-  const team1PitcherName = document.querySelector("#jsInfoTeam1PitcherName");
-
-  team1Name.innerHTML = team1.name;
-
-  for (let i = 0; i < team1PlayerName.length; i++) {
-    const player = team1.batterPlayers[i];
-    team1PlayerName[i].innerHTML = player.name;
-    team1PlayerAvrg[i].innerHTML = player.average;
-  }
-
-  team1PitcherName.innerHTML = team1.pitcher;
-
-  // 팀 2 정보 입력
-  const team2 = match.team2;
-  const team2Name = document.querySelector("#jsInfoTeam2Name");
-  const team2PlayerName = document.querySelectorAll(".jsInfoTeam2PlayerName");
-  const team2PlayerAvrg = document.querySelectorAll(".jsInfoTeam2PlayerAvrg");
-  const team2PitcherName = document.querySelector("#jsInfoTeam2PitcherName");
-
-  team2Name.innerHTML = team2.name;
-
-  for (let i = 0; i < team2PlayerName.length; i++) {
-    const player = team2.batterPlayers[i];
-    team2PlayerName[i].innerHTML = player.name;
-    team2PlayerAvrg[i].innerHTML = player.average;
-  }
-
-  team2PitcherName.innerHTML = team2.pitcher;
-}
-
-function showAndHideBox(e) {
-  const infoBox = document.querySelector("#jsInfoBox");
-  const playBox = document.querySelector("#jsPlayBox");
-  const inputBox = document.querySelector("#jsInputBox");
-
-  if (e.target.id === "jsShowInputBtn") {
-    infoBox.style.display = "none";
-    playBox.style.display = "none";
-    inputBox.style.display = "block";
-  } else if (e.target.id === "jsShowInfoBtn") {
-    if (match.team1) {
-      playBox.style.display = "none";
-      inputBox.style.display = "none";
-      infoBox.style.display = "block";
-      showInfo();
-    } else {
-      alert("팀 정보를 먼저 입력 해주세요.");
-    }
-  } else if (e.target.id === "jsShowPlayBtn") {
-    inputBox.style.display = "none";
-    infoBox.style.display = "none";
-    playBox.style.display = "block";
-  }
 }
 
 function main() {
